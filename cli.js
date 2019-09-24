b7fe7294f3faa28f26e54cc54884cf25df7b3b5d
Rui Aguiar <raguiar2@stanford.edu>
2019-09-23 17:22:57 -0700

    edited command line script to be more informative

--- cli.js
+++ cli.js
@@ -4,15 +4,26 @@ var exec = require('child_process').exec;
 // Grab CLI args
 const [,, ...args] = process.argv;
 
+if (args.length < 1) {
+  console.log("\nusage: create-node-project <target_dir>\n")
+  return;
+}
+
 const newDir = args[0];
+
+console.log("Creating directory and installing packages...");
 
 const command = `mkdir -p ${newDir} && cp -r ${__dirname}/files/. ${newDir} && cd ${newDir} && npm i`;
 
 dir = exec(command, function(err, stdout, stderr) {
     if (err) {
-      console.log("err is", err)
-      // should have err.code here?  
+      console.err(err)
+      console.log("Project was not successfully created. Please check error message above for more details.")
+      return;
     }
-    console.log(stdout);
-    console.log("Start your new nodejs app with `npm start` or `nodemon app.js`")
+    console.log("Project successfully created!");
+    console.log("Start your new nodejs app with npm start or nodemon app.js");
   });
+
+dir.stdout.pipe(process.stdout);
+dir.stderr.pipe(process.stderr);
